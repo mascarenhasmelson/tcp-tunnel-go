28499084bf6739f03ebe10e26ac255ff84d10c20
mascarenhasmelson <mail@0xmm.in>
2025-10-29 15:48:08 +0530

    fixed single Accept and EOF

--- main.go
+++ main.go
@@ -4,19 +4,20 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+//	"io"
 	"log"
 	"net"
 	"os"
 )
-
-var bindAddress = flag.String("bind-address", "127.0.0.1", "bind address")
-var localPort = flag.Int("local-port", 6000, "local port")
-var remotePort = flag.Int("remote-port", 0, "remote port")
-var remoteHost = flag.String("remote-host", "", "remote host")
-var bufferSize = flag.Int("buffer-size", 512, "buffer size")
-var displayLogs = flag.Bool("log", false, "log")
-var logFormat = flag.String("log-format", "raw", "log format (values: raw, hex)")
-
+var (
+     bindAddress = flag.String("bind-address", "127.0.0.1", "bind address")
+     localPort = flag.Int("local-port", 6000, "local port")
+     remotePort = flag.Int("remote-port", 0, "remote port")
+     remoteHost = flag.String("remote-host", "", "remote host")
+     bufferSize = flag.Int("buffer-size", 512, "buffer size")
+     displayLogs = flag.Bool("log", false, "log")
+     logFormat = flag.String("log-format", "raw", "log format (values: raw, hex)")
+)
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [options...]\n", os.Args[0])
@@ -28,73 +29,71 @@ func main() {
 
 	flag.Parse()
 
-	local, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *bindAddress, *localPort))
-
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *bindAddress, *localPort))
 	if err != nil {
 		log.Fatal("Failed to listen on local port: ", err)
 	}
+	log.Printf("Proxy listening on %s:%d, forwarding to %s:%d", *bindAddress, *localPort, *remoteHost, *remotePort)
 
-	localConn, err := local.Accept()
+	for {
+		localConn, err := listener.Accept()
+		if err != nil {
+			log.Print("Accept error: ", err)
+			continue
+		}
 
-	if err != nil {
-		log.Fatal("Failed to accept connection: ", err)
-	}
-
-	if *displayLogs == true {
-		log.Printf("Request from: %s", localConn.RemoteAddr())
-	}
-
-	remoteConn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *remoteHost, *remotePort))
-
-	if err != nil {
-		log.Fatal("Failed to connect to remote host: ", err)
-	}
-
-	if *displayLogs == true {
-		log.Printf("Connected to %s:%d", *remoteHost, *remotePort)
-	}
-
-	errChan := make(chan error)
-
-	go readLoop(localConn, remoteConn, errChan, true)
-	go readLoop(remoteConn, localConn, errChan, false)
-
-	select {
-	case err := <-errChan:
-		log.Fatal(err)
+		if *displayLogs {
+			log.Printf("Accepted connection from: %s", localConn.RemoteAddr())
+	        }
+		go handleConn(localConn)
 	}
 }
 
-func readLoop(from net.Conn, to net.Conn, errChan chan error, localToRemote bool) {
+func handleConn(localConn net.Conn) {
+	defer localConn.Close()
+
+	remoteConn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *remoteHost, *remotePort))
+	if err != nil {
+		log.Printf("Failed to connect to remote host: %v", err)
+		return
+	}
+	defer remoteConn.Close()
+	if *displayLogs {
+		log.Printf("Connected to remote %s:%d", *remoteHost, *remotePort)
+	}
+
+	errChan := make(chan error, 2) //bidirectional
+	go proxyLoop(localConn, remoteConn, errChan, true)
+	go proxyLoop(remoteConn, localConn, errChan, false)
+	<-errChan
+}
+
+func proxyLoop(from net.Conn, to net.Conn, errChan chan error, localToRemote bool) {
+	buffer := make([]byte, *bufferSize)
 	for {
-		data := make([]byte, *bufferSize)
-		bytes, err := from.Read(data)
-
-		if err != nil {
-			from.Close()
-			errChan <- err
-			return
-		}
-
-		if *displayLogs == true {
-			var logPrefix string
-			if localToRemote {
-				logPrefix = ">>> local to remote"
-			} else {
-				logPrefix = "<<< remote to local"
+		n, err := from.Read(buffer)
+		if n > 0 {
+			if *displayLogs {
+				var logPrefix string
+				if localToRemote {
+					logPrefix = ">>> local to remote"
+				} else {
+					logPrefix = "<<< remote to local"
+				}
+				if *logFormat == "hex" {
+					log.Print(logPrefix, "\n", hex.Dump(buffer[:n]))
+				} else {
+					log.Printf("%s\n%s", logPrefix, string(buffer[:n]))
+				}
 			}
 
-			if *logFormat == "hex" {
-				log.Print(logPrefix, "\n", hex.Dump(data[0:bytes]))
-			} else {
-				log.Print(logPrefix, "\n", string(data[0:bytes]))
+			_, err2 := to.Write(buffer[:n])
+			if err2 != nil {
+				errChan <- err2
+				return
 			}
 		}
-
-		_, err = to.Write(data[0:bytes])
-
 		if err != nil {
-			to.Close()
 			errChan <- err
 			return
 		}
